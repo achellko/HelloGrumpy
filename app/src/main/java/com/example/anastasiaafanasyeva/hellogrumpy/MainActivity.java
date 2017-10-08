4b619b330f0a467b18e35b6e2c732341bb728afe
Anastasia Afanasyeva <nastya@afanasyev.kiev.ua>
2017-10-08 18:47:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/anastasiaafanasyeva/hellogrumpy/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.anastasiaafanasyeva.hellogrumpy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mHelloTextView;
+    private EditText mNameEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mHelloTextView = (TextView)findViewById(R.id.textView);
+        mNameEditText = (EditText) findViewById(R.id.editText2);
+    }
+
+    public void onClick(View view) {
+        if(mNameEditText.getText().length() == 0) {
+            mHelloTextView.setText("Hello Grumpy!");
+        } else {
+            mHelloTextView.setText("Hello, " + mNameEditText.getText());
+        }
+    }
+}
