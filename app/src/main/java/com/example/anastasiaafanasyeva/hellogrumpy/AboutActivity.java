496fdd0e39d1a72d6fa1d13175fa81aca65f6a62
Anastasia Afanasyeva <nastya@afanasyev.kiev.ua>
2017-10-15 16:54:19 +0200

    "about author" into dialog theme

--- app/src/main/java/com/example/anastasiaafanasyeva/hellogrumpy/AboutActivity.java
+++ app/src/main/java/com/example/anastasiaafanasyeva/hellogrumpy/AboutActivity.java
@@ -3,16 +3,25 @@ package com.example.anastasiaafanasyeva.hellogrumpy;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 /**
  * Created by Anastasia Afanasyeva on 09-Oct-17.
  */
 
 public class AboutActivity extends Activity{
+
+    private TextView mHelloTextView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_about);
+
+        mHelloTextView = (TextView)findViewById(R.id.textAbout);
+        mHelloTextView.setText(R.string.about_text);
     }
+
+
 }
